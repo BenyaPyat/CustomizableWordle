779f2b8879315f7d415edfa4850738ac87033b9a
BenyaPyat <69654071+BenyaPyat@users.noreply.github.com>
2022-12-11 13:13:17 -0500

    Update ColorMenu.py

--- ColorMenu.py
+++ ColorMenu.py
@@ -1,3 +1,6 @@
+import matplotlib
+
+
 class ColorMenu:
     """class to get python specific color val"""
 
@@ -9,20 +12,7 @@ class ColorMenu:
         """converts color name to python color equivalent"""
         output_colors = []
 
-        # based off user input color return python specific name
+        # based off user input color return hex name
         for color in self.colors:
-            if 'green' == color:
-                output_colors.append('#76EEC6')
-            elif 'yellow' == color:
-                output_colors.append('#E3CF57')
-            elif 'black' == color:
-                output_colors.append('#483D8B')
-            elif 'white' in color:
-                output_colors.append('#F8F8FF')
-            elif 'blue' in color:
-                output_colors.append('#0000EE')
-            elif 'red' in color:
-                output_colors.append('#EE3B3B')
-            else:
-                pass
+            output_colors.append(matplotlib.colors.cnames[color])
         return output_colors
